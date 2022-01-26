f94a00dddb476c9af706cd3f7259dfd697fa52d9
Dapo Adeleke <adelekeoladapo@gmail.com>
2022-01-26 22:41:40 +0200

    Implement utility function to download web page

--- crawler/utils.go
+++ crawler/utils.go
@@ -1,13 +1,29 @@
 package crawler
 
 import (
+	"io"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func createDirectory(path string) (err error) {
 	if err = os.Mkdir(path, os.ModePerm); err != nil {
 		log.Printf("Error occurred while creating directory: %s. ", err)
+	}
+	return
+}
+
+func downloadWebPage(dir string, filename string, body io.Reader) (err error) {
+	file, err := os.Create(filepath.Join(dir, filepath.Base(filename+".html")))
+	if err != nil {
+		log.Println("Error occurred while creating file. ", err)
+		return
+	}
+	_, err = io.Copy(file, body)
+	if err != nil {
+		log.Println("Error occurred while downloading file. ", err)
+		return
 	}
 	return
 }
