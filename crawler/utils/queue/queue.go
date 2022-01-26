ed8aa38a45dbccc59729cc19b887fe0714c7f59d
Dapo Adeleke <adelekeoladapo@gmail.com>
2022-01-26 23:49:57 +0200

    Implement simple queue data structure

---
+++ crawler/utils/queue/queue.go
@@ -0,0 +1,46 @@
+package queue
+
+import "errors"
+
+type Queue interface {
+	Push(item interface{}) error
+	Pop() (interface{}, error)
+	Peek() (interface{}, error)
+	Size() int
+}
+
+type SimpleQueue struct {
+	items []interface{}
+}
+
+func (o *SimpleQueue) Push(item interface{}) (err error) {
+	o.items = append(o.items, item)
+	return
+}
+
+func (o *SimpleQueue) Pop() (item interface{}, err error) {
+	item, err = o.Peek()
+	if err != nil {
+		return
+	}
+	o.items = o.items[:len(o.items)-1]
+	return
+}
+
+func (o *SimpleQueue) Peek() (item interface{}, err error) {
+	if o.Size() == 0 {
+		err = errors.New("empty que")
+		return
+	}
+	item = o.items[len(o.items)-1]
+	return
+}
+
+func (o *SimpleQueue) Size() (size int) {
+	size = len(o.items)
+	return
+}
+
+func GetSimpleQueue() Queue {
+	return &SimpleQueue{}
+}
