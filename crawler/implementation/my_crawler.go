4b52af42c733e8e5d6c769b7e0abc55f569049bd
Dapo Adeleke <adelekeoladapo@gmail.com>
2022-01-27 19:55:25 +0200

    Crawler works fine

--- crawler/implementation/my_crawler.go
+++ crawler/implementation/my_crawler.go
@@ -1,14 +1,85 @@
 package implementation
 
 import (
+	"errors"
+	"fmt"
 	"github.com/dapo/webcrawler/crawler"
+	"github.com/dapo/webcrawler/crawler/utils"
+	"github.com/dapo/webcrawler/crawler/utils/queue"
+	"log"
+	"path"
+	"strings"
 )
 
 type MyCrawler struct {
+	pendingUrls queue.SimpleQueue
+	crawledUrls []string
 }
 
-func (o *MyCrawler) Process(url, dir string) {
+func (o *MyCrawler) Process(startingUrl, downloadDir string) {
+	if err := utils.CreateDirectory(downloadDir); err != nil {
+		log.Fatal("Error occurred while creating directory ", err)
+	}
+	o.pendingUrls.Enqueue(startingUrl)
+	for !o.pendingUrls.Empty() {
+		url, err := o.pendingUrls.Dequeue()
+		if err != nil {
+			log.Println("Error occurred while fetching from queue. ", err)
+		}
+		strUrl := url.(string)
+		if o.isUrlCrawled(strUrl) {
+			continue
+		}
+		fmt.Println("Crawling . . .  ", url)
+		body, err := utils.LoadUrl(strUrl)
+		if err != nil {
+			log.Println("Error occurred while loading url ", strUrl)
+		}
+		filename := path.Base(strUrl)
+		err = utils.DownloadWebPage(downloadDir, filename, body)
+		body, _ = utils.LoadUrl(strUrl)
+		urls := utils.GetLinks(body)
+		o.updatePendingUrls(strUrl, urls)
+		o.crawledUrls = append(o.crawledUrls, strUrl)
+	}
+	fmt.Println(len(o.crawledUrls), " urls crawled successfully")
+}
 
+func (o *MyCrawler) isUrlCrawled(url string) bool {
+	for _, u := range o.crawledUrls {
+		if u == url {
+			return true
+		}
+	}
+	return false
+}
+
+func (o *MyCrawler) updatePendingUrls(baseUrl string, urls []string) {
+	for _, u := range urls {
+		goodUrl, err := prepareFullUrl(baseUrl, u)
+		if err == nil {
+			o.pendingUrls.Enqueue(goodUrl)
+		}
+	}
+}
+
+func prepareFullUrl(baseUrl, url string) (goodUrl string, err error) {
+	if (strings.HasPrefix(url, "www") || strings.HasPrefix(url, "http")) && !strings.HasPrefix(url, baseUrl) {
+		err = errors.New("bad url")
+	} else if (strings.HasPrefix(url, baseUrl)) && baseUrl != url {
+		goodUrl = url
+	} else if strings.HasPrefix(url, "/") && strings.HasSuffix(url, "/") {
+		goodUrl = baseUrl + url[:len(url)-1]
+	} else if strings.HasPrefix(url, "/") && !strings.HasSuffix(url, "/") {
+		goodUrl = baseUrl + url
+	} else if !strings.HasPrefix(url, "/") && !strings.HasSuffix(url, "/") {
+		goodUrl = baseUrl + "/" + url
+	} else if !strings.HasPrefix(url, "/") && strings.HasSuffix(url, "/") {
+		goodUrl = baseUrl + "/" + url[:len(url)-1]
+	} else {
+		err = errors.New("bad url")
+	}
+	return
 }
 
 func GetMyCrawler() crawler.Crawler {
